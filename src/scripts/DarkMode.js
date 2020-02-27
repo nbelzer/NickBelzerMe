ec2f53fd1c4671039c55c818490878086f901ed8
Nick Belzer <me@nickbelzer.me>
2020-02-27 10:36:46 +0100

    Update DarkMode script
    The script now follows the color-scheme preference of the OS/browser and
    allows the user to override it using the toggle. The toggle can also be
    used on devices where the color-scheme preference is not set by the OS/browser.

--- src/scripts/DarkMode.js
+++ src/scripts/DarkMode.js
@@ -1,32 +1,65 @@
-import { deleteCookie, getCookie, setCookie } from './CookieHandler';
+/* Little script to follow the color scheme preferences of the user (set in the os) with the ability to override. */
 
-const darkModeToggleId = 'darkModeToggle';
-const darkModeClassName = 'dark-mode';
-const darkModeCookieName = 'dark-mode';
-const darkModeCookieDays = 31;
+// Preferences
+const darkContainerId = 'body';
+const darkToggleId = 'darkModeToggle';
+const darkClass = 'dark-mode';
+const defaultTheme = 'light';
+const sessionStorageKey = 'dark-mode';
 
-/**
- * Toggles Dark Mode.
- *
- * Enables/Disables dark mode by adjusting the classList on the body element.
- */
-function toggleDarkMode() {
-	const bodyClasses = document.getElementById('body').classList;
-	if (bodyClasses.contains(darkModeClassName)) {
-		bodyClasses.remove(darkModeClassName);
-		deleteCookie(darkModeCookieName)
-	} else {
-		bodyClasses.add(darkModeClassName);
-		setCookie(darkModeCookieName, true, darkModeCookieDays)
-	}
+// State variables
+const container = document.getElementById(darkContainerId);
+let currentTheme = defaultTheme;
+
+
+function enableDarkMode() {
+    container.classList.add(darkClass);
+    currentTheme = 'dark';
 }
 
-// Attach the dark mode toggle to the element with the darkModeToggleId.
-document.getElementById(darkModeToggleId).onclick = () => {
-	toggleDarkMode();
-	return false; // such that the browser doesn't redirect to href attribute.
-};
-
-if (getCookie(darkModeCookieName) === 'true') {
-	toggleDarkMode()
+function enableLightMode() {
+    container.classList.remove(darkClass);
+    currentTheme = 'light';
 }
+
+function toggle() {
+    if (currentTheme === 'light') {
+        enableDarkMode();
+    } else if (currentTheme === 'dark') {
+        enableLightMode()
+    }
+}
+
+const colorSchemeDarkMatch = window.matchMedia('(prefers-color-scheme: dark)');
+const colorSchemeLightMatch = window.matchMedia('(prefers-color-scheme: light)');
+
+if (sessionStorage.getItem(sessionStorageKey)) {
+    const sessionPreference = sessionStorage.getItem(sessionStorageKey);
+    if (sessionPreference === 'dark') {
+        enableDarkMode();
+    } else if (sessionPreference === 'light') {
+        enableLightMode();
+    }
+} else {
+    // Set value based on current system color scheme.
+    if (colorSchemeDarkMatch.matches) {
+        enableDarkMode();
+    } else if (colorSchemeLightMatch.matches) {
+        enableLightMode()
+    }
+}
+
+// Add listeners for color scheme changes.
+colorSchemeDarkMatch.addListener( e => {
+    if (e.matches) enableDarkMode();
+});
+colorSchemeLightMatch.addListener( e => {
+    if (e.matches) enableLightMode();
+});
+
+// Add event listener to toggle.
+document.getElementById(darkToggleId).addEventListener('click', e => {
+    e.preventDefault();
+    toggle();
+    sessionStorage.setItem(sessionStorageKey, currentTheme);
+});
